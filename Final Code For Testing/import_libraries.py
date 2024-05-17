c6deda81637e591334d20d204cd2add52cd1f236
Dylan Holmwood <d21124331@mytudublin.ie>
2024-05-17 16:04:05 +0100

    GUI developer view

--- Final Code For Testing/import_libraries.py
+++ Final Code For Testing/import_libraries.py
@@ -3,17 +3,12 @@ import cv2
 import time
 import threading
 import queue
-import speech_recognition as sr
 from facenet_pytorch import InceptionResnetV1
 import torchvision.transforms as transforms
 from PIL import Image
 import mediapipe as mp
 import torch
-import pygame
-import pygame
-import tempfile
 import os
-import speech_recognition as sr
 from openai import OpenAI
 import logging
 import cProfile
@@ -25,7 +20,5 @@ import wave
 import os
 import numpy as np
 from playsound import playsound
-import psutil
-import GPUtil
 import re
 from scipy.signal import butter, lfilter
